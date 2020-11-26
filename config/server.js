949ed9358cbb1b9c0fb94e6b0f50d3b687d89af0
TungThai <tungtt@grand9.vn>
2020-11-26 22:58:00 +0700

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9dd909ce05fae832eb9e59afe38e1c55'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2d94ba02-4988-42ce-b4c0-7ace3cfc35df'
+};
